a5a5e947d03cdd3df442297acd239652af839806
Saulo Hachem <hycodex@gmail.com>
2014-08-01 15:35:48 -0300

    first version of hydrate

---
+++ hydrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from gi.repository import Notify
+from gi.repository import Gtk
+import sched, time
+
+s = sched.scheduler(time.time, time.sleep)
+
+Notify.init("Drink")
+notice = Notify.Notification.new(
+    'Time to Hydrate',
+    'Take a break and have some water'
+)
+
+def showAlert(sc):
+    notice.show()
+    s.enter(900, 1, showAlert, (s,))
+    
+
+s.enter(900, 1, showAlert, (s,))
+s.run()
