a7e2c99797897510f83678d509205b981cc27a69
alissonrgs <alissonrgs1993@gmail.com>
2018-10-12 16:51:40 -0300

    Add commands with argparse and add option to drink coffee

--- hydrate.py
+++ hydrate.py
@@ -3,47 +3,54 @@
 import pgi
 pgi.install_as_gi()
 
+import argparse
 import os
+import sched
 import sys
-import sched, time
+import time
 
+from datetime import datetime, timedelta
 from gi.repository import Notify, Gtk, GdkPixbuf
 from random import randint
-from datetime import datetime, timedelta
 
-MAX_IMAGES = 2
 DELAY = 1200  # in seconds
+MAX_IMAGES = 2
 PRIORITY = 1
 
 
-if len(sys.argv) > 1:
-    DELAY = int(sys.argv[1])
-
-print('DELAY:', DELAY, 'seconds')
+parser = argparse.ArgumentParser(description='Hydrate')
+parser.add_argument('beverage',
+    type=str, choices=['water', 'coffee'], help='Select a beverage to notification')
+parser.add_argument('-d', '--delay',
+    type=int, help='Set an integer, a delay for notification')
+# parser.add_argument('-t', '--time',
+#     type=str, help='Set a time for notification in the format HH:MM:SS')
 
 path = os.getcwd()
-image_path = "{path}/images/agua_{image}.jpg"
-
-s = sched.scheduler(time.time, time.sleep)
+water_image_path = "{path}/images/water_{image}.jpg"
+coffee_image_path = "{path}/images/coffee_{image}.jpg"
 
 Notify.init("Drink")
-notice = Notify.Notification.new(
-    "Time to Hydrate",
-    "Take a break and have some water",
-    image_path.format(
-        path=path,
-        image=randint(1, MAX_IMAGES)
-    )
-)
 
-def showAlert():
+
+def showAlert(beverage, delay):
+    image_path = water_image_path if beverage == "water" else coffee_image_path
+
+    notice = Notify.Notification.new(
+        "Time to Hydrate",
+        f"Take a break and have some {beverage}",
+        image_path.format(
+            path=path,
+            image=randint(1, MAX_IMAGES)
+        )
+    )
     notice.show()
 
     icon = image_path.format(
         path=path,
         image=randint(1, MAX_IMAGES)
     )
-    pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(icon, 25, 25)
+    pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(icon, 400, 400)
 
     notice.set_icon_from_pixbuf(icon=pixbuf)
 
@@ -51,7 +58,14 @@ def showAlert():
     next_notice = now + timedelta(seconds=DELAY)
 
     print('next notice:', next_notice.strftime('%H:%M:%S - %d/%m/%Y'))
-    s.enter(DELAY, PRIORITY, showAlert)
+    s.enter(delay, PRIORITY, showAlert, argument=(beverage, delay))
 
-s.enter(1, PRIORITY, showAlert)
-s.run()
+
+if __name__ == "__main__":
+    parse = parser.parse_args()
+    beverage = parse.beverage
+    delay = parse.delay
+
+    s = sched.scheduler(time.time, time.sleep)
+    s.enter(1, PRIORITY, showAlert, argument=(beverage, delay))
+    s.run()
